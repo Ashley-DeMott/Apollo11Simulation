63dbd018e9b19be6025b8ce3c122b325c6ebe43d
Galaticash <ash9280@gmail.com>
2023-09-27 13:51:24 -0600

    Add project files.

---
+++ point.cpp
@@ -0,0 +1,47 @@
+/***********************************************************************
+ * Source File:
+ *    Point : The representation of a position on the screen
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    Everything we need to know about a location on the screen.
+ ************************************************************************/
+
+#include "point.h"
+#include <cassert>
+
+/******************************************
+ * POINT : CONSTRUCTOR WITH X,Y
+ * Initialize the point to the passed position
+ *****************************************/
+Point::Point(double x, double y) : x(0.0), y(0.0)
+{
+   setX(x);
+   setY(y);
+}
+
+/******************************************
+ * POINT insertion
+ *       Display coordinates on the screen
+ *****************************************/
+std::ostream & operator << (std::ostream & out, const Point & pt)
+{
+   out << "(" << pt.getX() << ", " << pt.getY() << ")";
+   return out;
+}
+
+/*******************************************
+ * POINT extraction
+ *       Prompt for coordinates
+ ******************************************/
+std::istream & operator >> (std::istream & in, Point & pt)
+{
+   double x;
+   double y;
+   in >> x >> y;
+
+   pt.setX(x);
+   pt.setY(y);
+
+   return in;
+}
